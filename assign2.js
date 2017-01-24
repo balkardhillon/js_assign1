ed4f2bdbc6a4992db67d2e20918fec6749a14c12
balkardhillon <balkardhillon53@gmail.com>
2017-01-23 16:01:30 -0800

    Update assign2.js

--- assign2.js
+++ assign2.js
@@ -1,8 +1,8 @@
 const Val = process.argv
-let myArr = [Number(Val[2])]
-console.log("Third Value from process.argv : " + myArr)
+let bal1 = [Number(Val[2])]
+console.log("Third Value from process.argv : " + bal1)
 bal1.push(Number(Val[3]))
 let a = Number(Val[2])
 let b = Number(Val[3])
-let c =x + y
-console.log("Addition of 3rd and 4th commandline argument : " + z)
+let c =a+ b
+console.log("Addition of 3rd and 4th argument : " + c)
